a8753f837d4f0a65a9e2af3628adcdec7aa94946
Michael Levan <mlevan1992@gmail.com>
2020-04-29 18:32:31 -0400

    AKS cluster

---
+++ PowerShell-For-DevOps-Automation/AKS-And-ACI/New-AKSCluster.ps1
@@ -0,0 +1,18 @@
+param(
+    [string]$name,
+    [string]$resourceGroupName,
+    [string]$location,
+    [string]$kubernetesVersion,
+    [int]$nodeCount
+)
+
+$NewAZParams = @{
+    'Name'              = $name
+    'ResourceGroupName' = $resourceGroupName
+    'location'          = $location
+    'KubernetesVersion' = $kubernetesVersion
+    'NodeCount'         = $nodeCount
+    'Force'             = $true
+}
+
+New-AzAks @NewAZParams
