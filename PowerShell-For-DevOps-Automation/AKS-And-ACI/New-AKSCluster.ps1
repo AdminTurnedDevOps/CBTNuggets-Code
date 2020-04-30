c6106c055314d1cd09583d38f5a061ee3e040e51
Michael Levan <mlevan1992@gmail.com>
2020-04-30 15:35:20 -0400

    Changed from powershell to az cli

--- PowerShell-For-DevOps-Automation/AKS-And-ACI/New-AKSCluster.ps1
+++ PowerShell-For-DevOps-Automation/AKS-And-ACI/New-AKSCluster.ps1
@@ -1,8 +1,8 @@
 param(
-    [string]$name = 'cbtaks92',
-    [string]$resourceGroupName = "CBTAzure",
-    [string]$location = "eastus",
-    [string]$kubernetesVersion = "1.16.7",
+    [string]$name,
+    [string]$resourceGroupName,
+    [string]$location,
+    [string]$kubernetesVersion,
     [int]$nodeCount = 1,
     [string]$SshKeyValue
 )
