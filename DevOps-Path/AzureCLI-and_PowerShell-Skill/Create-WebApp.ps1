e604c4d9ae2fc5968e92389db0dea9ad3354c049
Michael Levan <mlevan1992@outlook.com>
2020-05-07 12:55:18 -0400

    Create and list updated

--- DevOps-Path/AzureCLI-and_PowerShell-Skill/Create-WebApp.ps1
+++ DevOps-Path/AzureCLI-and_PowerShell-Skill/Create-WebApp.ps1
@@ -1,1 +1,9 @@
-az webapp list --query "[].hostNames"
+az appservice plan create --name "pythonwebapp92plan" `
+                          --resource-group "CBTAzure" `
+                          --is-linux `
+                          --sku "FREE" `
+
+az webapp create --name "pythonwebapp92" `
+                 --plan "pythonwebapp92plan" `
+                 --resource-group "CBTAzure" `
+                 --runtime "python|3.7"
--- DevOps-Path/AzureCLI-and_PowerShell-Skill/List-WebApp.ps1
+++ DevOps-Path/AzureCLI-and_PowerShell-Skill/List-WebApp.ps1
@@ -1,9 +1,2 @@
-az appservice plan create --name "pythonwebapp92plan" `
-                          --resource-group "CBTAzure" `
-                          --is-linux `
-                          --sku "FREE" `
+az webapp list --query "[].hostNames"
 
-az webapp create --name "pythonwebapp92" `
-                 --plan "pythonwebapp92plan" `
-                 --resource-group "CBTAzure" `
-                 --runtime "python|3.7"
