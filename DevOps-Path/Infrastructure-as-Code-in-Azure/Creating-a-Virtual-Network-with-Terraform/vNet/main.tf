953fa2e7698a1746939b820f84bc7e2432884969
Michael Levan <mlevan1992@gmail.com>
2020-05-21 12:16:57 -0400

    Terraform for DevOps path

---
+++ DevOps-Path/Infrastructure-as-Code-in-Azure/Creating-a-Virtual-Network-with-Terraform/vNet/main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+    version = "2.0.0"
+    subscription_id = var.subscriptionID
+
+    features {}
+}
+
+resource "azurerm_resource_group" "CBTDevRG" {
+    name = var.resourceGroupName
+    location = var.location
+}
+
+resource "azurerm_virtual_network" "CBTDev-vnet" {
+  name                = "CBTDev-vnet"
+  location            = var.location
+  resource_group_name = var.resourceGroupName
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["8.8.8.8", "8.8.4.4"]
+
+  tags = {
+    environment = "Dev"
+  }
+}
