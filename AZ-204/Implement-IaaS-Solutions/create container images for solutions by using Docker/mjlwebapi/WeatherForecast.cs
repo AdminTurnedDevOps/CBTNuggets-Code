5e512f2491a4657c4c7a202f95ea77809a4dc683
Michael Levan <mlevan1992@gmail.com>
2020-06-20 13:39:16 -0400

    Docker configurations

---
+++ AZ-204/Implement-IaaS-Solutions/create container images for solutions by using Docker/mjlwebapi/WeatherForecast.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace mjlwebapi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public class MikesSickSite
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
+
