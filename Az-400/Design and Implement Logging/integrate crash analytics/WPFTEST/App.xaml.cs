fe05ddfaab12abc8bfa51b71b340febd9c28fcf4
Michael Levan <mlevan1992@gmail.com>
2020-07-02 11:42:40 -0400

    Web API example

---
+++ Az-400/Design and Implement Logging/integrate crash analytics/WPFTEST/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace WPFTEST
+{
+    public partial class App : Application
+    {
+        static void OnStartup()
+        {
+            AppCenter.Start("b0c63a79-103c-4bcb-8c68-cf76267e87f8",
+            typeof(Analytics), typeof(Crashes));
+        }
+
+    }
+}
